4076f12051600f9638e96bca40781439a944282d
Rahul kumar <149350884+rahulkumar2112k@users.noreply.github.com>
2024-09-28 16:50:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ const body = document.querySelector('body');
 buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
 
-    // this.classList.toggle('active');
-  
-    if (e.target.id === 'grey') {
+    if (e.target.id === 'orange') {
       body.style.backgroundColor = e.target.id;
     }
     if (e.target.id === 'white') {
@@ -16,7 +14,7 @@ buttons.forEach(function (button) {
     if (e.target.id === 'blue') {
       body.style.backgroundColor = e.target.id;
     }
-    if (e.target.id === 'yellow') {
+    if (e.target.id === 'green') {
       body.style.backgroundColor = e.target.id;
     }
     
